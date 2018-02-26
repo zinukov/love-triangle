e129b12006d0849a76574ffd5140f3b5d69eea3f
zinukov <zinukovsasha@gmail.com>
2018-02-26 18:06:34 +0300

    implemet func

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let trianglesCount, s1, s2, s3;
+    trianglesCount = 0;
+    for( let i=0; i<preferences.length; i++){
+        s1 = preferences[i]-1;
+        s2 = preferences[s1]-1;
+        s3 = preferences[s2]-1;
+        if ((s3 == i) && (s1 != s2) && (s2 != s3) && (s1 != s3)){
+            trianglesCount++;
+        }
+    }
+    if (trianglesCount > 0) {
+        trianglesCount = trianglesCount / 3;
+    }
+    return trianglesCount;
 };
